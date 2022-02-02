35c92e067e13bc18f0288ed8404e1aeaf74387a3
Matilda Mared <matildamared@live.se>
2022-02-02 09:05:11 +0100

    add styling and content to project section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -13,14 +13,23 @@ function Projects() {
 					<ScreenshotWrapper>
 						<Screenshot src={instablamScreenshot} />
 					</ScreenshotWrapper>
-					<Description>
-						Instablam är en PWA där användaren med hjälp av enhetens kamera tar
-						foton och adderar passande filter. Fotona sparas och finns
-						tillgängliga i ett galleri så att användaren kan bläddra bland alla
-						tagna bilder. När det kommer till denna app är jag särskilt stolt
-						över arbetet jag har lagt ner för att göra innehållet
-						tillgänglighetsanpassat.
-					</Description>
+					<div>
+						<Description>
+							Instablam är en PWA där användaren med hjälp av enhetens kamera
+							tar foton och adderar passande filter. Fotona sparas och finns
+							tillgängliga i ett galleri så att användaren kan bläddra bland
+							alla tagna bilder. När det kommer till denna app är jag särskilt
+							stolt över arbetet jag har lagt ner för att göra innehållet
+							tillgänglighetsanpassat.
+						</Description>
+						<TechnologyList>
+							<Technology>HTML</Technology>
+							<Technology>CSS</Technology>
+							<Technology>JavaScript</Technology>
+							<Technology>React</Technology>
+							<Technology>styled-components</Technology>
+						</TechnologyList>
+					</div>
 				</ProjectContent>
 			</Project>
 		</Wrapper>
@@ -43,7 +52,7 @@ const ProjectHeading = styled.h3`
 	text-align: right;
 	font-size: 1.2rem;
 	font-family: var(--font-secondary);
-	margin-bottom: 1.5rem;
+	margin-bottom: 2rem;
 	position: relative;
 
 	&::after {
@@ -75,12 +84,33 @@ const Description = styled.p`
 const ScreenshotWrapper = styled.figure`
 	display: flex;
 	width: 100%;
-  min-width: 300px;
+	min-width: 300px;
 	max-width: 500px;
 	border-radius: 0.5rem;
 	overflow: hidden;
 	box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
 	margin-right: 1rem;
+`;
+
+const TechnologyList = styled.ul`
+	list-style-type: none;
+	padding: 0;
+  display: flex;
+  justify-content: flex-end;
+  font-family: var(--font-secondary);
+  font-size: .8rem;
+  color: var(--color-purple);
+`;
+
+const Technology = styled.li`
+	display: inline-block;
+  margin: .25rem;
+  padding: 1px 3px;
+  background-color: rgba(255, 255, 255, .05);
+
+  &:last-child {
+    margin-right: 0;
+  }
 `;
 
 export default Projects;
